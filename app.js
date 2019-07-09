5a31a77f3ef66f9a843af27a3b6deab947267b1c
harishbompally <harish_bompally@baxter.com>
2019-07-09 18:51:25 +0530

    added random port code based on host environment

--- app.js
+++ app.js
@@ -12,23 +12,8 @@ app.get("/", function(req, res, next) {
   res.render("index", { page: "Home", menuId: "home" });
 });
 
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "I am also reading"
-//   });
-// });
+const port = process.env.PORT || 3000
 
-app.listen("1992", () => {
-  console.log("connected to 1992 port");
+app.listen(port, () => {
+  console.log(`Server running at port `+port);
 });
-
-
-// var scrollToElement = require('scroll-to-element');
-// //scrollToElement('#workdetail');
- 
-// // with options
-// scrollToElement('.workdetails', {
-//     offset: 0,
-//     ease: 'out-bounce',
-//     duration: 1500
-// });
